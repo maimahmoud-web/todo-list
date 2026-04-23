513ec57b455fb418e1e48acca15a6eaab36b4276
maimahmoud-web <Maymaymay43877>
2026-04-23 23:34:52 +0200

    changes in gui

--- src/App.js
+++ src/App.js
@@ -1,24 +1,89 @@
-import logo from './logo.svg';
+
 import './App.css';
+import { useState } from "react";
+
+
+function Taskbtn({ tasks, setTasks, input, setInput }) {
+  return (
+    <div className='task-container'>
+      <button 
+        className='task-btn'
+        onClick={() => {
+          if (input.trim() === "") return; // prevent empty task
+          setTasks([...tasks, input]);
+          setInput("");
+        }}
+      >
+        Add task
+      </button>
+    </div>
+  );
+}
+
+function Todo(){
+  return(
+    <>
+    <div className='Todo'>
+      <Task task="me"
+      date="5:23 AM, 01/06/2022" />
+    </div>
+    
+    </>
+  )
+}
+
+function Task({task ,date}) {
+  return (
+    <div className="task-item">
+      
+      <div className="left">
+        <input type="checkbox" />
+
+        <div>
+          <p className="task-title">
+            {task}
+          </p>
+          <span className="task-date">
+            {date}
+          </span>
+        </div>
+      </div>
+
+      <div className="right">
+        <button>🗑️</button>
+        <button>✏️</button>
+      </div>
+
+    </div>
+  );
+}
 
 function App() {
+
+const [tasks, setTasks] = useState([]);
+const [input, setInput] = useState("");
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      <div className='Todo-Write'>
+        <h1>TODO LIST</h1>
+      </div>
+
+      <input 
+      onChange={(e) => setInput(e.target.value)}
+      placeholder='enter your task'
+      />
+
+<div className="task-section">
+     <Taskbtn 
+  tasks={tasks} 
+  setTasks={setTasks} 
+  input={input} 
+  setInput={setInput}
+/>
+      <Todo />
+</div>
+    </>
   );
 }
 
